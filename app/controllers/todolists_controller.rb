4ffbf82e1ee4cb036f9392e93e2f06680450f2dc
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-11-15 15:06:16 +0900

    [finish]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,23 +1,30 @@
 class TodolistsController < ApplicationController
 
-  def new
-  	@list = List.new
-  end
+    def new
+  	     @list = List.new
+    end
 
-  def create
+    def create
         # ストロングパラメーターを使用
          list = List.new(list_params)
         # DBへ保存する
          list.save
-        # トップ画面へリダイレクト
-        redirect_to '/top'
+        
+         redirect_to todolist_path(list.id)
     end
 
- 
+    def index
+        @lists = List.all
+    end
+
+    def show
+        @list = List.find(params[:id]) 
+    end
+
+
 
 
     private
-   
     def list_params
         params.require(:list).permit(:title, :body)
     end
