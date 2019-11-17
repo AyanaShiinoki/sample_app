c15fe74666749652dec69f87fd77d50edaf3020f
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-11-17 15:08:07 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a2e699e8a99caac2026ecd500b8fb7f02bde7b4053eb948bcd6a8b04c7db992d7abb6913268133576e69d1be566814f5da71fee12522265e3e5f4b8e6625fa98'
